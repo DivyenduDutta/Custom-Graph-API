6f47ef11eb8839f184cbeb16aed6bca6bb821df3
DivyenduDutta <connect2divyendu@gmail.com>
2018-11-24 21:46:34 +0530

    Added new Client to use DepthFirstSearchPaths API

---
+++ src/main/java/com/divyendu/GraphClient01.java
@@ -0,0 +1,38 @@
+package com.divyendu;
+
+import java.util.List;
+import com.divyendu.undirectedgraph.UndirectedGraph;
+import com.divyendu.undirectedgraph.tasks.DepthFirstSearchPaths;
+
+public class GraphClient01 {
+
+	public static final int NUMBER_OF_VERTICES = 6;
+	public static final int SOURCE = 4;
+	public static final int DESTINATION = 0;
+	
+	public static void main(String[] args) {
+		
+		//create an empty Undirected Graph with NUMBER_OF_VERTICES vertices
+		UndirectedGraph udGraph = new UndirectedGraph(NUMBER_OF_VERTICES);
+		//lets add edges
+		udGraph.addEdge(0, 1); 
+		udGraph.addEdge(0, 2);
+		udGraph.addEdge(0, 5);
+		udGraph.addEdge(1, 2);
+		udGraph.addEdge(2, 3);
+		udGraph.addEdge(2, 4);
+		udGraph.addEdge(3, 4);
+		udGraph.addEdge(3, 5);
+		
+		/* Start of DFS path client*/
+		DepthFirstSearchPaths dfsPathClient = new DepthFirstSearchPaths(udGraph, SOURCE);
+		List<Integer> path = dfsPathClient.pathTo(DESTINATION);
+		System.out.println("Path from "+SOURCE+" to "+DESTINATION+" is: ");
+		System.out.print(SOURCE+" -> ");
+		for(int vertex : path) {
+			System.out.print(vertex+" -> ");
+		}
+		System.out.print(DESTINATION);
+	}
+
+}
