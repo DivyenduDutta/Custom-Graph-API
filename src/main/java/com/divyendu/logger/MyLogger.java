36ae39e2de2520226de4dd1d06b5a65c20d4ef12
DivyenduDutta <connect2divyendu@gmail.com>
2019-01-19 23:07:37 +0530

    adding logging

---
+++ src/main/java/com/divyendu/logger/MyLogger.java
@@ -0,0 +1,33 @@
+package com.divyendu.logger;
+
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import com.divyendu.logger.formatter.MyFormatter;
+import com.divyendu.queue.Queue;
+
+public class MyLogger {
+	Logger logger = Logger.getLogger(MyLogger.class.getName());	//get logger instance
+	LogManager logManager = LogManager.getLogManager();
+	
+	public MyLogger() {
+		try {
+			logManager.readConfiguration(Queue.class.getResourceAsStream("/mylogging.properties"));
+			logger.setLevel(Level.FINE);
+			logger.setUseParentHandlers(false);
+			ConsoleHandler conHandler = new ConsoleHandler();
+			conHandler.setFormatter(new MyFormatter());
+		    logger.addHandler(conHandler);
+			} catch(IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+
+	public Logger getLogger() {
+		return logger;
+	}
+	
+}
---
+++ src/main/java/com/divyendu/logger/formatter/MyFormatter.java
@@ -0,0 +1,14 @@
+package com.divyendu.logger.formatter;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class MyFormatter extends Formatter {
+	@Override
+    public String format(LogRecord record) {
+        return record.getSourceClassName()+"::"
+                +record.getSourceMethodName()+"::"
+                +record.getMessage()+"\n";
+    }
+
+}
