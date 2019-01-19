89869934bf5868eca9159185be51ac8968ce83f3
DivyenduDutta <connect2divyendu@gmail.com>
2019-01-19 21:03:26 +0530

    Adding Node class used in Queue

---
+++ src/main/java/com/divyendu/node/Node.java
@@ -0,0 +1,39 @@
+package com.divyendu.node;
+
+/**
+ * Custom Node with generic type data - To be used in Custom Queue implementation
+ * 
+ * @author divyendu
+ *
+ */
+public class Node<T> {
+	
+	private T value;
+	private Node<T> nextNode;
+	private Node<T> prevNode;
+	
+	/**
+	 * Parameterised constructor which creates a node
+	 * 
+	 */
+	public Node(T value) {
+		this.value = value;
+		this.nextNode = null;
+	}
+
+	public void setNextNode(Node<T> nextNode) {
+		this.nextNode = nextNode;
+	}
+	
+
+	public Node<T> getNextNode() {
+		return nextNode;
+	}
+
+	public T getValue() {
+		return value;
+	}
+	
+	
+	
+}
